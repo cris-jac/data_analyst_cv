b7610eab79422cb227768bcf9902afc0008be7d2
cris-jac <cristian25co@hotmail.com>
2024-02-28 01:12:23 -0300

    Update HashRouter

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, BrowserRouter as Router } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <HashRouter>
-
-    </HashRouter> */}
-    <Router basename={process.env.APP_URL}>
+    <HashRouter>
+    {/* <Router basename={process.env.APP_URL}> */}
       <App />
-    </Router>
+    {/* </Router> */}
+    </HashRouter>
   </React.StrictMode>
 );
 
